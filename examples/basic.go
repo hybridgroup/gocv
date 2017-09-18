c63f060e72cff795c3e79eb795a0f247e99ec3c2
deadprogram <ron@hybridgroup.com>
2017-09-18 23:53:21 +0200

    Example that can perform very simple video capture
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- examples/basic.go
+++ examples/basic.go
@@ -7,11 +7,27 @@ import (
 )
 
 func main() {
-	deviceID := 1
+	deviceID := 0
 	vcap := opencv3.NewVideoCapture()
 	defer vcap.Delete()
 
 	if ok := vcap.OpenDevice(int(deviceID)); !ok {
 		fmt.Errorf("error opening device: %v", deviceID)
+	}
+
+	// streaming, capture from vcap
+	buf := opencv3.NewMatVec3b()
+	defer buf.Delete()
+	fmt.Printf("start reading camera device: %v", deviceID)
+	for {
+		if ok := vcap.Read(buf); !ok {
+			fmt.Errorf("cannot read a new file (device no: %d)", deviceID)
+			return
+		}
+		if buf.Empty() {
+			continue
+		}
+
+		fmt.Println("frame")
 	}
 }
