8d62f8498df1f89e5d60e22b7ae9809f5bc654db
deadprogram <ron@hybridgroup.com>
2017-09-19 10:23:05 +0200

    Combining files to match original OpenCV structure
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- videocapture.cpp
+++ videocapture.cpp
@@ -1,5 +1,6 @@
 #include "videocapture.h"
 
+// VideoWriter
 VideoCapture VideoCapture_New() {
     return new cv::VideoCapture();
 }
@@ -36,4 +37,31 @@ void VideoCapture_Grab(VideoCapture v, int skip) {
     for (int i =0; i < skip; i++) {
         v->grab();
     }
+}
+
+// VideoWriter
+VideoWriter VideoWriter_New() {
+    return new cv::VideoWriter();
+}
+  
+void VideoWriter_Delete(VideoWriter vw) {
+    delete vw;
+}
+  
+void VideoWriter_Open(VideoWriter vw, const char* name, double fps, int width,
+      int height) {
+    vw->open(name, CV_FOURCC('M', 'J', 'P', 'G'), fps, cv::Size(width, height), true);
+}
+  
+void VideoWriter_OpenWithMat(VideoWriter vw, const char* name, double fps,
+      MatVec3b img) {
+    vw->open(name, CV_FOURCC('M', 'J', 'P', 'G'), fps, img->size(), true);
+}
+  
+int VideoWriter_IsOpened(VideoWriter vw) {
+    return vw->isOpened();
+}
+  
+void VideoWriter_Write(VideoWriter vw, MatVec3b img) {
+    *vw << *img;
 }
--- videocapture.h
+++ videocapture.h
@@ -10,10 +10,13 @@ extern "C" {
 
 #ifdef __cplusplus
 typedef cv::VideoCapture* VideoCapture;
+typedef cv::VideoWriter* VideoWriter;
 #else
 typedef void* VideoCapture;
+typedef void* VideoWriter;
 #endif
 
+// VideoCapture
 VideoCapture VideoCapture_New();
 void VideoCapture_Delete(VideoCapture v);
 int VideoCapture_Open(VideoCapture v, const char* uri);
@@ -23,6 +26,16 @@ void VideoCapture_Set(VideoCapture v, int prop, int param);
 int VideoCapture_IsOpened(VideoCapture v);
 int VideoCapture_Read(VideoCapture v, MatVec3b buf);
 void VideoCapture_Grab(VideoCapture v, int skip);
+
+// VideoWriter
+VideoWriter VideoWriter_New();
+void VideoWriter_Delete(VideoWriter vw);
+void VideoWriter_Open(VideoWriter vw, const char* name, double fps, int width,
+  int height);
+void VideoWriter_OpenWithMat(VideoWriter vw, const char* name, double fps,
+  MatVec3b img);
+int VideoWriter_IsOpened(VideoWriter vw);
+void VideoWriter_Write(VideoWriter vw, MatVec3b img);
 
 #ifdef __cplusplus
 }
