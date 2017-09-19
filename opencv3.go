0a3e5794bfa9d8dbe863638514747599aa208109
deadprogram <ron@hybridgroup.com>
2017-09-19 12:02:40 +0200

    Improve package comment for future godocs
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- opencv3.go
+++ opencv3.go
@@ -1,2 +1,4 @@
-// package opencv3 is a wrapper around the latest OpenCV 3.x computer vision libs
+// Package opencv3 is a wrapper around the latest OpenCV 3.x computer vision libs.
+// It uses various C-styles wrappers around the C++ classes to avoid the complexity
+// of SWIG with such a large and complex library.
 package opencv3
