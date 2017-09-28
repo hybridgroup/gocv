20b96b95c0735d8487c458a46d57868f83b2d5b8
deadprogram <ron@hybridgroup.com>
2017-09-28 12:28:51 +0200

    Move fonts where they truly belong
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- imgproc.go
+++ imgproc.go
@@ -31,6 +31,21 @@ func Rectangle(img Mat, r Rect, c Scalar) {
 	C.Rectangle(img.p, cRect, sColor)
 }
 
+// Based on the enum HersheyFonts
+// Only a subset of Hershey fonts
+// <http://sources.isc.org/utils/misc/hershey-font.txt> are supported
+const (
+	FontHersheySimplex       = 0  //!< normal size sans-serif font
+	FontHersheyPlain         = 1  //!< small size sans-serif font
+	FontHersheyDuplex        = 2  //!< normal size sans-serif font (more complex than FontHersheySIMPLEX)
+	FontHersheyComplex       = 3  //!< normal size serif font
+	FontHersheyTriplex       = 4  //!< normal size serif font (more complex than FontHersheyCOMPLEX)
+	FontHersheyComplexSmall  = 5  //!< smaller version of FontHersheyCOMPLEX
+	FontHersheyScriptSimplex = 6  //!< hand-writing style font
+	FontHersheyScriptComplex = 7  //!< more complex variant of FontHersheySCRIPT_SIMPLEX
+	FontItalic               = 16 //!< flag for italic font
+)
+
 // GetTextSize returns the size required to draw text using a specific font face,
 // scale, and thickness.
 func GetTextSize(text string, fontFace int, fontScale float64, thickness int) Size {
