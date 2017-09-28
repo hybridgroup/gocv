523ad63431272ddbcf30a67f61a317dcd8b76fe7
deadprogram <ron@hybridgroup.com>
2017-09-28 11:53:21 +0200

    Test for imgproc GetTextSize()
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- imgproc_test.go
+++ imgproc_test.go
@@ -4,6 +4,19 @@ import (
 	"testing"
 )
 
-func TestImgproc(t *testing.T) {
-	t.Skip("Tests needed")
+func TestRect(t *testing.T) {
+	t.Skip("Test needed")
+}
+func TestGetTextSize(t *testing.T) {
+	size := GetTextSize("test", FontHersheySimplex, 1.2, 1)
+	if size.Width != 72 {
+		t.Error("Invalid text size width")
+	}
+
+	if size.Height != 26 {
+		t.Error("Invalid text size height")
+	}
+}
+func TestPutText(t *testing.T) {
+	t.Skip("Test needed")
 }
