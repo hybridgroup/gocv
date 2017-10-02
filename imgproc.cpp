dff02cccceb1335feb534bc271584334fd5bd8dc
deadprogram <ron@hybridgroup.com>
2017-10-02 20:09:00 +0200

    Format C++ code mostly same as the Go code
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- imgproc.cpp
+++ imgproc.cpp
@@ -1,12 +1,10 @@
 #include "imgproc.h"
 
-void CvtColor(Mat src, Mat dst, int code)
-{
+void CvtColor(Mat src, Mat dst, int code) {
     cv::cvtColor(*src, *dst, code);
 }
 
-void GaussianBlur(Mat src, Mat dst, Size ps, double sX, double sY, int bt)
-{
+void GaussianBlur(Mat src, Mat dst, Size ps, double sX, double sY, int bt) {
     cv::Size sz(ps.width, ps.height);
     cv:GaussianBlur(*src, *dst, sz, sX, sY, bt);
 }
@@ -17,16 +15,14 @@ void Rectangle(Mat img, Rect r, Scalar color, int thickness) {
         c, thickness, CV_AA);
 }
 
-struct Size GetTextSize(const char* text, int fontFace, double fontScale, int thickness)
-{
+struct Size GetTextSize(const char* text, int fontFace, double fontScale, int thickness) {
     cv::Size sz = cv::getTextSize(text, fontFace, fontScale, thickness, NULL);
     Size size = {sz.width, sz.height};
     return size;
 }
 
 void PutText(Mat img, const char* text, Point org, int fontFace, double fontScale, 
-    Scalar color, int thickness)
-{
+    Scalar color, int thickness) {
     cv::Point pt(org.x, org.y);
     cv::Scalar c = cv::Scalar(color.val1, color.val2, color.val3, color.val4);
     cv::putText(*img, text, pt, fontFace, fontScale, c, thickness);
