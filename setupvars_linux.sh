34232c6c5f24545ffdf6358aef29153252f21e40
Marc-Antoine Ruel <maruel@chromium.org>
2017-10-02 09:43:39 -0400

    Merge setup_vars_*.sh into env.sh
    This reduces the different flows users have to follow based on their OS.
    
    Remove the +x bit on the script, since it is not meant to be executed but
    sourced. Remove the shebang for the same reason.

--- setupvars_linux.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export CGO_CPPFLAGS="-I/usr/local/include"
-export CGO_CXXFLAGS="--std=c++1z"
-export CGO_LDFLAGS="-L/usr/local/lib -lopencv_core -lopencv_videoio -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_objdetect -lopencv_calib3d"
