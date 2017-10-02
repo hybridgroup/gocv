a346836cc224a641475b98cab9487e0b0c74e79f
Marc-Antoine Ruel <maruel@chromium.org>
2017-10-02 09:43:39 -0400

    Merge setup_vars_*.sh into env.sh
    This reduces the different flows users have to follow based on their OS.
    
    Remove the +x bit on the script, since it is not meant to be executed but
    sourced. Remove the shebang for the same reason.

---
+++ env.sh
@@ -0,0 +1,14 @@
+uname_val="$(uname)"
+if [[ "$uname_val" == "Darwin" ]]; then
+  export CGO_CPPFLAGS="-I/usr/local/Cellar/opencv/3.3.0/include -I/usr/local/Cellar/opencv/3.3.0/include/opencv2"
+  export CGO_CXXFLAGS="--std=c++1z -stdlib=libc++"
+  export CGO_LDFLAGS="-L/usr/local/Cellar/opencv/3.3.0/lib -lopencv_core -lopencv_videoio -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_objdetect -lopencv_calib3d"
+  echo "Environment variables configured for OSX"
+elif [[ "$uname_val" == "Linux" ]]; then
+  export CGO_CPPFLAGS="-I/usr/local/include"
+  export CGO_CXXFLAGS="--std=c++1z"
+  export CGO_LDFLAGS="-L/usr/local/lib -lopencv_core -lopencv_videoio -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_objdetect -lopencv_calib3d"
+  echo "Environment variables configured for Linux"
+else
+  echo "Unknown platform '$uname_val'!"
+fi
