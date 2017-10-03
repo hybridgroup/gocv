c58a8d371dfc9eb109e7cdf2cfb9722b9395e431
deadprogram <ron@hybridgroup.com>
2017-10-03 11:47:38 +0200

    Add tests for imgcodecs IMEncode
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- imgcodecs_test.go
+++ imgcodecs_test.go
@@ -12,5 +12,28 @@ func TestIMRead(t *testing.T) {
 }
 
 func TestIMWrite(t *testing.T) {
-	t.Skip("Tests needed")
+	img := IMRead("images/face-detect.jpg", IMReadColor)
+	if img.Empty() {
+		t.Error("Invalid read of Mat in IMWrite test")
+	}
+
+	result := IMWrite("/tmp/test.jpg", img)
+	if !result {
+		t.Error("Invalid write of Mat in IMWrite test")
+	}
+}
+
+func TestIMEncode(t *testing.T) {
+	img := IMRead("images/face-detect.jpg", IMReadColor)
+	if img.Empty() {
+		t.Error("Invalid Mat in IMEncode test")
+	}
+
+	buf, err := IMEncode(".jpg", img)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(buf) != 43209 {
+		t.Errorf("Wrong buffer size in IMEncode test. Should have been %v\n", len(buf))
+	}
 }
