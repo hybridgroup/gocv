598f34068e1d4839030c131de26e700ee1d28eaf
DeadNumbers <wagn3r@ya.ru>
2017-10-11 13:52:09 +0300

    Update env.sh
    fix env vars for Arch linux

--- env.sh
+++ env.sh
@@ -5,9 +5,15 @@ if [[ "$uname_val" == "Darwin" ]]; then
   export CGO_LDFLAGS="-L/usr/local/Cellar/opencv/3.3.0_3/lib -lopencv_core -lopencv_videoio -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_objdetect -lopencv_calib3d"
   echo "Environment variables configured for OSX"
 elif [[ "$uname_val" == "Linux" ]]; then
-  export CGO_CPPFLAGS="-I/usr/local/include"
-  export CGO_CXXFLAGS="--std=c++1z"
-  export CGO_LDFLAGS="-L/usr/local/lib -lopencv_core -lopencv_videoio -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_objdetect -lopencv_calib3d"
+        if [[ -f /etc/pacman.conf ]]; then
+                export CGO_CPPFLAGS="-I/usr/include"
+                export CGO_CXXFLAGS="--std=c++1z"
+                export CGO_LDFLAGS="-L/lib64 -lopencv_core -lopencv_videoio -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_objdetect -lopencv_calib3d"
+        else
+                export CGO_CPPFLAGS="-I/usr/local/include"
+                export CGO_CXXFLAGS="--std=c++1z"
+                export CGO_LDFLAGS="-L/usr/local/lib -lopencv_core -lopencv_videoio -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_objdetect -lopencv_calib3d"
+        fi
   echo "Environment variables configured for Linux"
 else
   echo "Unknown platform '$uname_val'!"
