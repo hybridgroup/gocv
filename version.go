544104ece678364b4106dbaefd83c4e82c6e6b39
deadprogram <ron@hybridgroup.com>
2017-11-08 21:39:45 +0100

    Prepare for release v0.4.2
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- version.go
+++ version.go
@@ -7,7 +7,7 @@ package gocv
 import "C"
 
 // GoCVVersion of this package, for display purposes.
-const GoCVVersion = "0.4.0"
+const GoCVVersion = "0.4.2"
 
 // Version returns the current golang package version
 func Version() string {
