35c0bab7365dbe9eef29fa22c0dc76529acf05e2
deadprogram <ron@hybridgroup.com>
2017-11-14 07:21:09 +0100

    osx: update env path to match latest OpenCV 3.3.1_1 from Homebrew
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- env.sh
+++ env.sh
@@ -1,8 +1,8 @@
 uname_val="$(uname)"
 if [[ "$uname_val" == "Darwin" ]]; then
-  export CGO_CPPFLAGS="-I/usr/local/Cellar/opencv/3.3.1/include -I/usr/local/Cellar/opencv/3.3.1/include/opencv2"
+  export CGO_CPPFLAGS="-I/usr/local/Cellar/opencv/3.3.1_1/include -I/usr/local/Cellar/opencv/3.3.1_1/include/opencv2"
   export CGO_CXXFLAGS="--std=c++1z -stdlib=libc++"
-  export CGO_LDFLAGS="-L/usr/local/Cellar/opencv/3.3.1/lib -lopencv_core -lopencv_videoio -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_objdetect -lopencv_calib3d -lopencv_video"
+  export CGO_LDFLAGS="-L/usr/local/Cellar/opencv_1/3.3.1_1/lib -lopencv_core -lopencv_videoio -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_objdetect -lopencv_calib3d -lopencv_video"
   echo "Environment variables configured for OSX"
 elif [[ "$uname_val" == "Linux" ]]; then
         if [[ -f /etc/pacman.conf ]]; then
