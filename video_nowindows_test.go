d55c9b48facf993b21f0634259d84ff5e3acabeb
deadprogram <ron@hybridgroup.com>
2017-11-17 10:33:59 +0100

    video: separate tests for algoritms that fail on Windows
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ video_nowindows_test.go
@@ -0,0 +1,49 @@
+// +build !windows
+// These tests are not currently able to run on Windows. They pass on both
+// Linux and OS X, however.
+
+package gocv
+
+import (
+	"testing"
+)
+
+func TestMOG2(t *testing.T) {
+	img := IMRead("images/face.jpg", IMReadColor)
+	if img.Empty() {
+		t.Error("Invalid Mat in MOG2 test")
+	}
+	defer img.Close()
+
+	dst := NewMat()
+	defer dst.Close()
+
+	mog2 := NewBackgroundSubtractorMOG2()
+	defer mog2.Close()
+
+	mog2.Apply(img, dst)
+
+	if dst.Empty() {
+		t.Error("Error in TestMOG2 test")
+	}
+}
+
+func TestKNN(t *testing.T) {
+	img := IMRead("images/face.jpg", IMReadColor)
+	if img.Empty() {
+		t.Error("Invalid Mat in KNN test")
+	}
+	defer img.Close()
+
+	dst := NewMat()
+	defer dst.Close()
+
+	knn := NewBackgroundSubtractorKNN()
+	defer knn.Close()
+
+	knn.Apply(img, dst)
+
+	if dst.Empty() {
+		t.Error("Error in TestKNN test")
+	}
+}
--- video_test.go
+++ video_test.go
@@ -1,51 +1,9 @@
-// +build !windows
-
 package gocv
 
 import (
 	"image"
 	"testing"
 )
-
-func TestMOG2(t *testing.T) {
-	img := IMRead("images/face.jpg", IMReadColor)
-	if img.Empty() {
-		t.Error("Invalid Mat in MOG2 test")
-	}
-	defer img.Close()
-
-	dst := NewMat()
-	defer dst.Close()
-
-	mog2 := NewBackgroundSubtractorMOG2()
-	defer mog2.Close()
-
-	mog2.Apply(img, dst)
-
-	if dst.Empty() {
-		t.Error("Error in TestMOG2 test")
-	}
-}
-
-func TestKNN(t *testing.T) {
-	img := IMRead("images/face.jpg", IMReadColor)
-	if img.Empty() {
-		t.Error("Invalid Mat in KNN test")
-	}
-	defer img.Close()
-
-	dst := NewMat()
-	defer dst.Close()
-
-	knn := NewBackgroundSubtractorKNN()
-	defer knn.Close()
-
-	knn.Apply(img, dst)
-
-	if dst.Empty() {
-		t.Error("Error in TestKNN test")
-	}
-}
 
 func TestCalcOpticalFlowFarneback(t *testing.T) {
 	img1 := IMRead("images/face.jpg", IMReadColor)
