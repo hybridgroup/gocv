3737e10369fc4adabd2f528dad475f8c1ab1824b
deadprogram <ron@hybridgroup.com>
2017-11-20 16:29:56 +0100

    core: update to v0.5.0
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- version.go
+++ version.go
@@ -7,7 +7,7 @@ package gocv
 import "C"
 
 // GoCVVersion of this package, for display purposes.
-const GoCVVersion = "0.4.2"
+const GoCVVersion = "0.5.0"
 
 // Version returns the current golang package version
 func Version() string {
