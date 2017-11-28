d24f3e5f79f83f17d538b2a6a81129fa2248fbda
deadprogram <ron@hybridgroup.com>
2017-11-28 16:54:11 +0100

    xfeatures2d: add SIFT and SURF algorithms from OpenCV contrib
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ contrib/README.md
@@ -0,0 +1,13 @@
+# Using OpenCV Contrib
+
+GoCV support for OpenCV's Contrib can be found here in the "gocv.io/x/gocv/contrib" package.
+
+For more information about OpenCV Contrib, please go to:
+
+https://github.com/opencv/opencv_contrib
+
+## How to use
+
+```go
+// code here...
+```
--- env.sh
+++ env.sh
@@ -3,17 +3,17 @@ if [[ "$uname_val" == "Darwin" ]]; then
   CVPATH=$(brew info opencv | sed -n "4p" | sed -e "s/ (.*//g")
   export CGO_CPPFLAGS="-I$CVPATH/include -I$CVPATH/include/opencv2"
   export CGO_CXXFLAGS="--std=c++1z -stdlib=libc++"
-  export CGO_LDFLAGS="-L$CVPATH/lib -lopencv_core -lopencv_videoio -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_objdetect -lopencv_features2d -lopencv_video"
+  export CGO_LDFLAGS="-L$CVPATH/lib -lopencv_core -lopencv_videoio -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_objdetect -lopencv_features2d -lopencv_video -lopencv_xfeatures2d"
   echo "Environment variables configured for OSX"
 elif [[ "$uname_val" == "Linux" ]]; then
         if [[ -f /etc/pacman.conf ]]; then
                 export CGO_CPPFLAGS="-I/usr/include"
                 export CGO_CXXFLAGS="--std=c++1z"
-                export CGO_LDFLAGS="-L/lib64 -lopencv_core -lopencv_videoio -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_objdetect -lopencv_features2d -lopencv_video"
+                export CGO_LDFLAGS="-L/lib64 -lopencv_core -lopencv_videoio -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_objdetect -lopencv_features2d -lopencv_video -lopencv_xfeatures2d"
         else
                 export CGO_CPPFLAGS="-I/usr/local/include"
                 export CGO_CXXFLAGS="--std=c++1z"
-                export CGO_LDFLAGS="-L/usr/local/lib -lopencv_core -lopencv_videoio -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_objdetect -lopencv_features2d -lopencv_video"
+                export CGO_LDFLAGS="-L/usr/local/lib -lopencv_core -lopencv_videoio -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_objdetect -lopencv_features2d -lopencv_video -lopencv_xfeatures2d"
         fi
   echo "Environment variables configured for Linux"
 else
