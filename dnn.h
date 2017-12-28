7a1592b51ffdb52a32b97359c0e06b040a569543
deadprogram <ron@hybridgroup.com>
2017-12-28 20:14:02 +0100

    dnn: first pass at adding Caffe support
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ dnn.h
@@ -0,0 +1,30 @@
+#ifndef _OPENCV3_DNN_H_
+#define _OPENCV3_DNN_H_
+
+#include <stdbool.h>
+
+#ifdef __cplusplus
+#include <opencv2/opencv.hpp>
+#include <opencv2/dnn.hpp>
+extern "C" {
+#endif
+
+#include "core.h"
+
+#ifdef __cplusplus
+typedef cv::dnn::Net* Net;
+#else
+typedef void* Net;
+#endif
+
+Net Net_ReadNetFromCaffe(const char* prototxt, const char* caffeModel);
+Mat Net_BlobFromImage(Mat image, double scalefactor, Size size, Scalar mean, bool swapRB, bool crop);
+bool Net_Empty(Net net);
+void Net_SetInput(Net net, Mat blob, const char* name);
+Mat Net_Forward(Net net, const char* outputName);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //_OPENCV3_DNN_H_
---
+++ dnn_test.go
@@ -0,0 +1,52 @@
+package gocv
+
+import (
+	"image"
+	"os"
+	"testing"
+)
+
+func TestCaffe(t *testing.T) {
+	path := os.Getenv("GOCV_CAFFE_TEST_FILES")
+	if path == "" {
+		t.Skip("Unable to locate Caffe model files for tests")
+	}
+
+	net := ReadNetFromCaffe(path+"/bvlc_googlenet.prototxt", path+"/bvlc_googlenet.caffemodel")
+	if net.Empty() {
+		t.Errorf("Unable to load Caffe model")
+	}
+
+	img := IMRead("images/space_shuttle.jpg", IMReadColor)
+	if img.Empty() {
+		t.Error("Invalid Mat in Caffe test")
+	}
+	defer img.Close()
+
+	blob := BlobFromImage(img, 1.0, image.Pt(224, 244), NewScalar(104, 117, 123, 0), false, false)
+	if blob.Empty() {
+		t.Error("Invalid blob in Caffe test")
+	}
+	defer blob.Close()
+
+	net.SetInput(blob, "data")
+	prob := net.Forward("prob")
+	if prob.Empty() {
+		t.Error("Invalid prob in Caffe test")
+	}
+
+	probMat := prob.Reshape(1, 1)
+	_, maxVal, minLoc, maxLoc := MinMaxLoc(probMat)
+
+	if maxVal != 0.999905 {
+		t.Errorf("Caffe maxVal incorrect: %v\n", maxVal)
+	}
+
+	if minLoc.X != 793 || minLoc.Y != 0 {
+		t.Errorf("Caffe minLoc incorrect: %v\n", minLoc)
+	}
+
+	if maxLoc.X != 812 || maxLoc.Y != 0 {
+		t.Errorf("Caffe maxLoc incorrect: %v\n", maxLoc)
+	}
+}
