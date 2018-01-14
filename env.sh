0b4232fe11cdebe9bbed31d45113f45b813ff367
Unknown <egoavara@gmail.com>
2018-01-15 00:32:48 +0900

    #57 Issue fix

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 uname_val="$(uname)"
 if [[ "$uname_val" == "Darwin" ]]; then
-  CVPATH=$(brew info opencv | sed -n "4p" | sed -e "s/ (.*//g")
+  CVPATH=$(brew info opencv | grep -E "opencv/3\.([3-9]|[1-9]\d{1,})" | sed -e "s/ (.*//g")
   export CGO_CPPFLAGS="-I$CVPATH/include -I$CVPATH/include/opencv2"
   export CGO_CXXFLAGS="--std=c++1z -stdlib=libc++"
   export CGO_LDFLAGS="-L$CVPATH/lib -lopencv_core -lopencv_videoio -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_objdetect -lopencv_features2d -lopencv_video -lopencv_xfeatures2d"
