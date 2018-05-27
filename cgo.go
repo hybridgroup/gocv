d2ff2c0d850226849d2e64e42dea95f1f7a024fa
Ron Evans <ron@hybridgroup.com>
2018-05-27 10:55:11 +0200

    build: we actually do need cpp 11 or greater for a warning free build on Linux
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

--- cgo.go
+++ cgo.go
@@ -6,8 +6,7 @@ package gocv
 
 /*
 #cgo !windows pkg-config: opencv
-#cgo darwin   CXXFLAGS:   --std=c++11
-#cgo windows  CXXFLAGS:   --std=c++11
+#cgo CXXFLAGS:   --std=c++11
 #cgo windows  CPPFLAGS:   -IC:/opencv/build/install/include
 #cgo windows  LDFLAGS:    -LC:/opencv/build/install/x64/mingw/lib -lopencv_core341 -lopencv_face341 -lopencv_videoio341 -lopencv_imgproc341 -lopencv_highgui341 -lopencv_imgcodecs341 -lopencv_objdetect341 -lopencv_features2d341 -lopencv_video341 -lopencv_dnn341 -lopencv_xfeatures2d341 -lopencv_plot341 -lopencv_tracking341 -lopencv_img_hash341
 */
