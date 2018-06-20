f3c56377e07e05f4ca725dea5dbc7254f675e040
Ron Evans <ron@hybridgroup.com>
2018-06-20 13:41:38 +0200

    example: show how to use either Caffe or Tensorflow for DNN object detection
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

--- cmd/dnn-detection/main.go
+++ cmd/dnn-detection/main.go
@@ -1,12 +1,24 @@
 // What it does:
 //
-// This example uses a deep neural network to perform face detection.
+// This example uses a deep neural network to perform object detection.
+// It can be used with either the Caffe face tracking or Tensorflow object detection models that are
+// included with OpenCV 3.4
+//
+// To perform face tracking with the Caffe model:
 //
 // Download the model file from:
 // https://github.com/opencv/opencv_3rdparty/raw/dnn_samples_face_detector_20170830/res10_300x300_ssd_iter_140000.caffemodel
 //
-// You will also need the prototxt file:
+// You will also need the prototxt config file:
 // https://raw.githubusercontent.com/opencv/opencv/master/samples/dnn/face_detector/deploy.prototxt
+//
+// To perform object tracking with the Tensorflow model:
+//
+// Download and extract the model file named "frozen_inference_graph.pb" from:
+// http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v1_coco_2017_11_17.tar.gz
+//
+// You will also need the pbtxt config file:
+// https://gist.githubusercontent.com/dkurt/45118a9c57c38677b65d6953ae62924a/raw/b0edd9e8c992c25fe1c804e77b06d20a89064871/ssd_mobilenet_v1_coco_2017_11_17.pbtxt
 //
 // How to run:
 //
@@ -21,6 +33,7 @@ import (
 	"image"
 	"image/color"
 	"os"
+	"path/filepath"
 
 	"gocv.io/x/gocv"
 )
@@ -33,8 +46,8 @@ func main() {
 
 	// parse args
 	deviceID := os.Args[1]
-	proto := os.Args[2]
-	model := os.Args[3]
+	model := os.Args[2]
+	config := os.Args[3]
 	backend := gocv.NetBackendDefault
 	if len(os.Args) > 4 {
 		backend = gocv.ParseNetBackend(os.Args[4])
@@ -59,24 +72,28 @@ func main() {
 	img := gocv.NewMat()
 	defer img.Close()
 
-	// open DNN face tracking model
-	net := gocv.ReadNet(model, proto)
+	// open DNN object tracking model
+	net := gocv.ReadNet(model, config)
 	if net.Empty() {
-		fmt.Printf("Error reading network model from : %v %v\n", model, proto)
+		fmt.Printf("Error reading network model from : %v %v\n", model, config)
 		return
 	}
 	defer net.Close()
 	net.SetPreferableBackend(gocv.NetBackendType(backend))
 	net.SetPreferableTarget(gocv.NetTargetType(target))
 
-	// make sure we have valid output layer type
-	outs := net.GetUnconnectedOutLayers()
-	layer := net.GetLayer(outs[1])
-	layerType := layer.GetType()
-	layer.Close()
-	if layerType != "DetectionOutput" {
-		fmt.Printf("Error unknown output layer type %v\n", layerType)
-		return
+	var ratio float64
+	var mean gocv.Scalar
+	var swapRGB bool
+
+	if filepath.Ext(model) == ".caffemodel" {
+		ratio = 1.0
+		mean = gocv.NewScalar(104, 177, 123, 0)
+		swapRGB = false
+	} else {
+		ratio = 1.0 / 127.5
+		mean = gocv.NewScalar(127.5, 127.5, 127.5, 0)
+		swapRGB = true
 	}
 
 	fmt.Printf("Start reading camera device: %v\n", deviceID)
@@ -91,13 +108,13 @@ func main() {
 		}
 
 		// convert image Mat to 300x300 blob that the object detector can analyze
-		blob := gocv.BlobFromImage(img, 1.0, image.Pt(300, 300), gocv.NewScalar(104, 177, 123, 0), false, false)
+		blob := gocv.BlobFromImage(img, ratio, image.Pt(300, 300), mean, swapRGB, false)
 
 		// feed the blob into the detector
 		net.SetInput(blob, "")
 
 		// run a forward pass thru the network
-		prob := net.Forward("detection_out")
+		prob := net.Forward("")
 
 		performDetection(&img, prob)
 
