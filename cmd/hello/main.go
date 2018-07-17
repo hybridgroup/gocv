ad9cfcd94e1c8f7358d8e432f4f15b5ca882e1fa
Ron Evans <ron@hybridgroup.com>
2018-07-17 10:53:58 +0200

    examples: add 'hello, video' example to repo
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

---
+++ cmd/hello/main.go
@@ -0,0 +1,18 @@
+// +build example
+package main
+
+import (
+	"gocv.io/x/gocv"
+)
+
+func main() {
+	webcam, _ := gocv.OpenVideoCapture(0)
+	window := gocv.NewWindow("Hello")
+	img := gocv.NewMat()
+
+	for {
+		webcam.Read(&img)
+		window.IMShow(img)
+		window.WaitKey(1)
+	}
+}
