116580adca5ee7dbf8d1307c9072f5094051116f
Ron Evans <ron@hybridgroup.com>
2018-08-27 15:48:12 -0600

    Prepare for release v0.16.0
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

--- version.go
+++ version.go
@@ -7,7 +7,7 @@ package gocv
 import "C"
 
 // GoCVVersion of this package, for display purposes.
-const GoCVVersion = "0.15.0"
+const GoCVVersion = "0.16.0"
 
 // Version returns the current golang package version
 func Version() string {
