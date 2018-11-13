7253ebdc0afcb8500e21a24804db5cae3ad9a2f3
Ron Evans <ron@hybridgroup.com>
2018-11-13 16:59:33 +0100

    build: use use opencv4 file for pkg-config
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

--- contrib/cgo.go
+++ contrib/cgo.go
@@ -5,7 +5,7 @@ package contrib
 // Changes here should be mirrored in gocv/cgo.go.
 
 /*
-#cgo !windows pkg-config: opencv
+#cgo !windows pkg-config: opencv4
 #cgo CXXFLAGS:   --std=c++11
 #cgo windows  CPPFLAGS:   -IC:/opencv/build/install/include
 #cgo windows  LDFLAGS:    -LC:/opencv/build/install/x64/mingw/lib -lopencv_core400 -lopencv_face400 -lopencv_videoio400 -lopencv_imgproc400 -lopencv_highgui400 -lopencv_imgcodecs400 -lopencv_objdetect400 -lopencv_features2d400 -lopencv_video400 -lopencv_dnn400 -lopencv_xfeatures2d400 -lopencv_plot400 -lopencv_tracking400 -lopencv_img_hash400
