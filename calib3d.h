91dae95aa5f6be0cfec99583b7e4bce90eb26107
linyongzuo <414747746@qq.com>
2019-02-10 22:40:10 +0800

    add opencv func

--- calib3d.h
+++ calib3d.h
@@ -15,6 +15,9 @@ extern "C" {
 void Fisheye_UndistortImage(Mat distorted, Mat undistorted, Mat k, Mat d);
 void Fisheye_UndistortImageWithParams(Mat distorted, Mat undistorted, Mat k, Mat d, Mat knew, Size size);
 
+void InitUndistortRectifyMap(Mat cameraMatrix,Mat distCoeffs,Mat r,Mat newCameraMatrix,Size size,int m1type,Mat map1,Mat map2);
+Mat GetOptimalNewCameraMatrixWithParams(Mat cameraMatrix,Mat distCoeffs,Size size,double alpha,Size newImgSize,Rect* validPixROI,bool centerPrincipalPoint);
+
 #ifdef __cplusplus
 }
 #endif
