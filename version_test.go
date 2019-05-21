60903bbeac6fdf2402fba33e19fae000f70b8234
Ron Evans <ron@hybridgroup.com>
2019-05-21 11:22:49 +0200

    test: set version test to look for OpenCV 4.1.x
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

--- version_test.go
+++ version_test.go
@@ -8,7 +8,7 @@ import (
 func TestVersions(t *testing.T) {
 	ocvv := OpenCVVersion()
 
-	if !strings.Contains(ocvv, "4.0") {
+	if !strings.Contains(ocvv, "4.1") {
 		t.Error("Wrong version of OpenCV:", ocvv)
 	}
 
