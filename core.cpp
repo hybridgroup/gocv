08c4f18ac21cccd268e1db0b3bac2cd75b40aa76
31434116 <33805710+31434116@users.noreply.github.com>
2019-06-12 17:46:01 +0800

    Update core.cpp
    Add Mat_Compute, Mat_SimilarityTransform function

--- core.cpp
+++ core.cpp
@@ -379,6 +379,10 @@ void Mat_Compare(Mat src1, Mat src2, Mat dst, int ct) {
     cv::compare(*src1, *src2, *dst, ct);
 }
 
+void Mat_Compute(Mat a, Mat w, Mat u, Mat vt, int flags) {
+    cv::SVD::compute(*a, *w, *u, *vt, flags);
+}
+
 int Mat_CountNonZero(Mat src) {
     return cv::countNonZero(*src);
 }
@@ -712,3 +716,241 @@ int64 GetCVTickCount() {
 double GetTickFrequency() {
     return cv::getTickFrequency();
 }
+
+// --------------------add by yoda.guo--------------------------------
+// xy = [x y]
+// XY = [[x  y  1  0]
+//       [y -x  0  1]]
+cv::Mat stitch(const cv::Mat xy)
+{
+    int M = xy.rows;
+    cv::Mat x = xy.col(0);
+    cv::Mat y = xy.col(1);
+    cv::Mat zeros = cv::Mat::zeros(M, 1, CV_32FC1);
+    cv::Mat ones = cv::Mat::ones(M, 1, CV_32FC1);
+
+    cv::Mat XY(2 * M, 4, CV_32FC1);
+
+    cv::Mat XY_up = XY.rowRange(0, M);
+    x.copyTo(XY_up.col(0));
+    y.copyTo(XY_up.col(1));
+    ones.copyTo(XY_up.col(2));
+    zeros.copyTo(XY_up.col(3));
+
+    cv::Mat XY_down = XY.rowRange(M, 2 * M);
+    y.copyTo(XY_down.col(0));
+    cv::Mat _x = x * -1;
+    _x.copyTo(XY_down.col(1));
+    zeros.copyTo(XY_down.col(2));
+    ones.copyTo(XY_down.col(3));
+
+    return std::move(XY);
+}
+
+int rank(const cv::Mat mat)
+{
+    cv::Mat1d w; // singular values
+    cv::SVD::compute(mat, w);
+    return cv::countNonZero(w > 0.f);
+}
+
+cv::Mat tformfwd(const cv::Mat trans, const cv::Mat uv)
+{
+    cv::Mat UV = cv::Mat::ones(uv.rows, 3, uv.type());
+    uv.copyTo(UV.colRange(0, 2));
+    cv::Mat XY = UV * trans;
+    cv::Mat xy;
+    XY.colRange(0, 2).copyTo(xy);
+    return std::move(xy);
+}
+
+// 2-norm of matrix = largest singular value of mat.
+double norm(const cv::Mat mat)
+{
+    cv::Mat1d w; // singular values
+    cv::SVD::compute(mat, w);
+    double min_v, max_v;
+    cv::minMaxIdx(w, &min_v, &max_v);
+    return max_v;
+}
+
+cv::Mat nonReflectiveSimilarity(const cv::Mat uv, const cv::Mat xy, int option_k = 2)
+{
+    int K = option_k;
+    int M = xy.rows;
+    cv::Mat X = stitch(xy);
+    cv::Mat U = cv::Mat(uv.t()).reshape(1, 2 * M);
+
+    // We know that X * r = U
+    CV_Assert(::rank(X) >= 2 * K); // unique solution
+    cv::Mat r(4, 1, CV_32FC1);
+    cv::solve(X, U, r, cv::DECOMP_SVD);
+
+    const float * pdata = r.ptr<float>(0);
+    cv::Mat tinv = (cv::Mat_<float>(3, 3) <<
+                    pdata[0], -pdata[1], 0,
+                    pdata[1],  pdata[0], 0,
+                    pdata[2],  pdata[3], 1);
+
+    return std::move(tinv.inv(cv::DECOMP_SVD));
+}
+
+cv::Mat findSimilarity(const cv::Mat uv, const cv::Mat xy, int option_k = 2)
+{
+    /// solve for trans1
+    cv::Mat trans1 = nonReflectiveSimilarity(uv, xy, option_k);
+    /// solve for trans2
+    // manually reflect the xy data across the Y-axis
+    cv::Mat xyR;
+    xy.copyTo(xyR);
+    xyR.col(0) *= -1;
+    cv::Mat trans2 = nonReflectiveSimilarity(uv, xyR, option_k);
+    // manually reflect the tfrom to undo the reflection done on xyR.
+    trans2.col(0) *= -1;
+    // print(trans1, "trans1");
+    // print(trans2, "trans2");
+
+    // Figure out if trans1 or trans2 is better
+    cv::Mat xy1 = tformfwd(trans1, uv);
+    double norm1 = ::norm(xy1 - xy);
+
+    cv::Mat xy2 = tformfwd(trans2, uv);
+    double norm2 = ::norm(xy2 - xy);
+
+    if (norm1 <= norm2)
+        return std::move(trans1);
+    else
+        return std::move(trans2);
+}
+
+Mat Mat_SimilarityTransform(Mat src, Mat dst)
+{
+    cv::Mat trans = findSimilarity(*src, *dst);
+    return new cv::Mat(std::move(trans));
+}
+
+//----insightface
+/*
+cv::Mat meanAxis0(const cv::Mat &src)
+{
+    int num = src.rows;
+    int dim = src.cols;
+
+    // x1 y1
+    // x2 y2
+
+    cv::Mat output(1,dim,CV_32F);
+    for(int i = 0 ; i <  dim; i ++)
+    {
+        float sum = 0 ;
+        for(int j = 0 ; j < num ; j++)
+        {
+            sum += src.at<float>(j,i);
+        }
+        output.at<float>(0,i) = sum/num;
+    }
+    return output;
+}
+
+cv::Mat elementwiseMinus(const cv::Mat &A,const cv::Mat &B)
+{
+    cv::Mat output(A.rows,A.cols,A.type());
+
+    assert(B.cols == A.cols);
+    if(B.cols == A.cols)
+    {
+        for(int i = 0 ; i <  A.rows; i ++)
+        {
+            for(int j = 0 ; j < B.cols; j++)
+            {
+               output.at<float>(i,j) = A.at<float>(i,j) - B.at<float>(0,j);
+            }
+        }
+    }
+    return output;
+}
+
+cv::Mat varAxis0(const cv::Mat &src)
+{
+    cv::Mat temp_ = elementwiseMinus(src,meanAxis0(src));
+    cv::multiply(temp_ ,temp_ ,temp_ );
+    return meanAxis0(temp_);
+}
+
+int MatrixRank(cv::Mat M)
+{
+    cv::Mat w, u, vt;
+    cv::SVD::compute(M, w, u, vt);
+    cv::Mat1b nonZeroSingularValues = w > 0.0001;
+    int rank = cv::countNonZero(nonZeroSingularValues);
+    return rank;
+}
+
+cv::Mat similarTransform(cv::Mat src,cv::Mat dst) {
+    int num = src.rows;
+    int dim = src.cols;
+    cv::Mat src_mean = meanAxis0(src);
+    cv::Mat dst_mean = meanAxis0(dst);
+    cv::Mat src_demean = elementwiseMinus(src, src_mean);
+    cv::Mat dst_demean = elementwiseMinus(dst, dst_mean);
+    cv::Mat A = (dst_demean.t() * src_demean) / static_cast<float>(num);
+    cv::Mat d(dim, 1, CV_32F);
+    d.setTo(1.0f);
+    if (cv::determinant(A) < 0) {
+        d.at<float>(dim - 1, 0) = -1;
+    }
+    cv::Mat T = cv::Mat::eye(dim + 1, dim + 1, CV_32F);
+    cv::Mat U, S, V;
+    cv::SVD::compute(A, S,U, V);
+
+        // the SVD function in opencv differ from scipy .
+    int rank = MatrixRank(A);
+    if (rank == 0) {
+        assert(rank == 0);
+    } else if (rank == dim - 1) {
+        if (cv::determinant(U) * cv::determinant(V) > 0) {
+            T.rowRange(0, dim).colRange(0, dim) = U * V;
+        } else {
+//           s = d[dim - 1]
+//           d[dim - 1] = -1
+//           T[:dim, :dim] = np.dot(U, np.dot(np.diag(d), V))
+//           d[dim - 1] = s
+            int s = d.at<float>(dim - 1, 0) = -1;
+            d.at<float>(dim - 1, 0) = -1;
+
+            T.rowRange(0, dim).colRange(0, dim) = U * V;
+            cv::Mat diag_ = cv::Mat::diag(d);
+            cv::Mat twp = diag_*V; //np.dot(np.diag(d), V.T)
+            cv::Mat B = cv::Mat::zeros(3, 3, CV_8UC1);
+            cv::Mat C = B.diag(0);
+            T.rowRange(0, dim).colRange(0, dim) = U* twp;
+            d.at<float>(dim - 1, 0) = s;
+        }
+    }
+    else{
+        cv::Mat diag_ = cv::Mat::diag(d);
+        cv::Mat twp = diag_*V.t(); //np.dot(np.diag(d), V.T)
+        cv::Mat res = U* twp; // U
+        T.rowRange(0, dim).colRange(0, dim) = -U.t()* twp;
+    }
+    cv::Mat var_ = varAxis0(src_demean);
+    float val = cv::sum(var_).val[0];
+    cv::Mat res;
+    cv::multiply(d,S,res);
+    float scale =  1.0/val*cv::sum(res).val[0];
+    T.rowRange(0, dim).colRange(0, dim) = - T.rowRange(0, dim).colRange(0, dim).t();
+    cv::Mat  temp1 = T.rowRange(0, dim).colRange(0, dim); // T[:dim, :dim]
+    cv::Mat  temp2 = src_mean.t(); //src_mean.T
+    cv::Mat  temp3 = temp1*temp2; // np.dot(T[:dim, :dim], src_mean.T)
+    cv::Mat temp4 = scale*temp3;
+    T.rowRange(0, dim).colRange(dim, dim+1)=  -(temp4 - dst_mean.t()) ;
+    T.rowRange(0, dim).colRange(0, dim) *= scale;
+    return T;
+}
+
+Mat Mat_SimilarityTransform(Mat src, Mat dst)
+{
+    cv::Mat trans = similarTransform(*src, *dst);
+    return new cv::Mat(std::move(trans));
+}
+*/
