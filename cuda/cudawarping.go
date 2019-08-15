2826bc38651d8cb3a4aa0f75981ca2f47245c983
Anubhav Patel <anubhavp28@gmail.com>
2019-08-15 21:07:45 +0530

    adds more methods from cuda image warping

--- cuda/cudawarping.go
+++ cuda/cudawarping.go
@@ -5,7 +5,10 @@ package cuda
 #include "cudawarping.h"
 */
 import "C"
-import "image"
+import (
+	"image"
+	"image/color"
+)
 
 // InterpolationFlags are bit flags that control the interpolation algorithm
 // that is used.
@@ -35,15 +38,39 @@ const (
 	InterpolationMax = 7
 )
 
+// BorderType type of border.
+type BorderType int
+
+const (
+	// BorderConstant border type
+	BorderConstant BorderType = 0
+
+	// BorderReplicate border type
+	BorderReplicate = 1
+
+	// BorderReflect border type
+	BorderReflect = 2
+
+	// BorderWrap border type
+	BorderWrap = 3
+
+	// BorderReflect101 border type
+	BorderReflect101 = 4
+
+	// BorderTransparent border type
+	BorderTransparent = 5
+
+	// BorderDefault border type
+	BorderDefault = BorderReflect101
+
+	// BorderIsolated border type
+	BorderIsolated = 16
+)
+
 // Resize resizes an image.
-// It resizes the image src down to or up to the specified size, storing the
-// result in dst. Note that src and dst may be the same image. If you wish to
-// scale by factor, an empty sz may be passed and non-zero fx and fy. Likewise,
-// if you wish to scale to an explicit size, a non-empty sz may be passed with
-// zero for both fx and fy.
 //
 // For further details, please see:
-// https://docs.opencv.org/master/da/d54/group__imgproc__transform.html#ga47a974309e9102f5f08231edc7e7529d
+// https://docs.opencv.org/master/db/d29/group__cudawarping.html#ga4f5fa0770d1c9efbadb9be1b92a6452a
 func Resize(src GpuMat, dst *GpuMat, sz image.Point, fx, fy float64, interp InterpolationFlags) {
 	pSize := C.struct_Size{
 		width:  C.int(sz.X),
@@ -51,5 +78,110 @@ func Resize(src GpuMat, dst *GpuMat, sz image.Point, fx, fy float64, interp Inte
 	}
 
 	C.CudaResize(src.p, dst.p, pSize, C.double(fx), C.double(fy), C.int(interp))
-	return
+}
+
+// Rotate rotates an image around the origin (0,0) and then shifts it.
+//
+// For further details, please see:
+// https://docs.opencv.org/master/db/d29/group__cudawarping.html#ga55d958eceb0f871e04b1be0adc6ef1b5
+func Rotate(src GpuMat, dst *GpuMat, sz image.Point, angle, xShift, yShift float64, interp InterpolationFlags) {
+	pSize := C.struct_Size{
+		width:  C.int(sz.X),
+		height: C.int(sz.Y),
+	}
+	C.CudaRotate(src.p, dst.p, pSize, C.double(angle), C.double(xShift), C.double(yShift), C.int(interp))
+}
+
+// Remap applies a generic geometrical transformation to an image.
+//
+// For further details, please see:
+// https://docs.opencv.org/master/db/d29/group__cudawarping.html#ga0ece6c76e8efa3171adb8432d842beb0
+func Remap(src GpuMat, dst, xmap, ymap *GpuMat, interpolation InterpolationFlags, borderMode BorderType, borderValue color.RGBA) {
+	bv := C.struct_Scalar{
+		val1: C.double(borderValue.B),
+		val2: C.double(borderValue.G),
+		val3: C.double(borderValue.R),
+		val4: C.double(borderValue.A),
+	}
+	C.CudaRemap(src.p, dst.p, xmap.p, ymap.p, C.int(interpolation), C.int(borderMode), bv)
+}
+
+// PyrDown blurs an image and downsamples it.
+//
+// For further details, please see:
+// https://docs.opencv.org/master/db/d29/group__cudawarping.html#ga9c8456de9792d96431e065f407c7a91b
+func PyrDown(src GpuMat, dst *GpuMat) {
+	C.CudaPyrDown(src.p, dst.p)
+}
+
+// PyrUp upsamples an image and then blurs it.
+//
+// For further details, please see:
+// https://docs.opencv.org/master/db/d29/group__cudawarping.html#ga2048da0dfdb9e4a726232c5cef7e5747
+func PyrUp(src GpuMat, dst *GpuMat) {
+	C.CudaPyrUp(src.p, dst.p)
+}
+
+// WarpPerspective applies a perspective transformation to an image.
+//
+// For further details, please see:
+// https://docs.opencv.org/master/db/d29/group__cudawarping.html#ga7a6cf95065536712de6b155f3440ccff
+func WarpPerspective(src GpuMat, dst *GpuMat, m GpuMat, sz image.Point, flags InterpolationFlags, borderType BorderType, borderValue color.RGBA) {
+	pSize := C.struct_Size{
+		width:  C.int(sz.X),
+		height: C.int(sz.Y),
+	}
+	bv := C.struct_Scalar{
+		val1: C.double(borderValue.B),
+		val2: C.double(borderValue.G),
+		val3: C.double(borderValue.R),
+		val4: C.double(borderValue.A),
+	}
+
+	C.CudaWarpPerspective(src.p, dst.p, m.p, pSize, C.int(flags), C.int(borderType), bv)
+}
+
+// WarpAffine applies an affine transformation to an image. For more parameters please check WarpAffineWithParams
+//
+// For further details, please see:
+// https://docs.opencv.org/master/db/d29/group__cudawarping.html#ga9e8dd9e73b96bdc8e27d85c0e83f1130
+func WarpAffine(src GpuMat, dst *GpuMat, m GpuMat, sz image.Point, flags InterpolationFlags, borderType BorderType, borderValue color.RGBA) {
+	pSize := C.struct_Size{
+		width:  C.int(sz.X),
+		height: C.int(sz.Y),
+	}
+	bv := C.struct_Scalar{
+		val1: C.double(borderValue.B),
+		val2: C.double(borderValue.G),
+		val3: C.double(borderValue.R),
+		val4: C.double(borderValue.A),
+	}
+
+	C.CudaWarpAffine(src.p, dst.p, m.p, pSize, C.int(flags), C.int(borderType), bv)
+}
+
+// BuildWarpAffineMaps builds transformation maps for affine transformation.
+//
+// For further details. please see:
+// https://docs.opencv.org/master/db/d29/group__cudawarping.html#ga63504590a96e4cc702d994281d17bc1c
+func BuildWarpAffineMaps(M GpuMat, inverse bool, sz image.Point, xmap, ymap *GpuMat) {
+	pSize := C.struct_Size{
+		width:  C.int(sz.X),
+		height: C.int(sz.Y),
+	}
+
+	C.CudaBuildWarpAffineMaps(M.p, C.bool(inverse), pSize, xmap.p, ymap.p)
+}
+
+// BuildWarpPerspectiveMaps builds transformation maps for perspective transformation.
+//
+// For further details, please see:
+// https://docs.opencv.org/master/db/d29/group__cudawarping.html#ga8d16e3003703bd3b89cca98c913ef864
+func BuildWarpPerspectiveMaps(M GpuMat, inverse bool, sz image.Point, xmap, ymap *GpuMat) {
+	pSize := C.struct_Size{
+		width:  C.int(sz.X),
+		height: C.int(sz.Y),
+	}
+
+	C.CudaBuildWarpPerspectiveMaps(M.p, C.bool(inverse), pSize, xmap.p, ymap.p)
 }
--- cuda/cudawarping.h
+++ cuda/cudawarping.h
@@ -11,7 +11,15 @@ extern "C" {
 #include "../core.h"
 #include "cuda.h"
 
-void  CudaResize(GpuMat src, GpuMat dst, Size dsize, double fx, double fy, int interp);
+void CudaResize(GpuMat src, GpuMat dst, Size dsize, double fx, double fy, int interp);
+void CudaPyrDown(GpuMat src, GpuMat dst);
+void CudaPyrUp(GpuMat src, GpuMat dst);
+void CudaBuildWarpAffineMaps(GpuMat M, bool inverse, Size dsize, GpuMat xmap, GpuMat ymap);
+void CudaBuildWarpPerspectiveMaps(GpuMat M, bool inverse, Size dsize, GpuMat xmap, GpuMat ymap);
+void CudaRemap(GpuMat src, GpuMat dst, GpuMat xmap, GpuMat ymap, int interp, int borderMode, Scalar borderValue);
+void CudaRotate(GpuMat src, GpuMat dst, Size dsize, double angle, double xShift, double yShift, int interp);
+void CudaWarpAffine(GpuMat src, GpuMat dst, GpuMat M, Size dsize, int flags, int borderMode, Scalar borderValue);
+void CudaWarpPerspective(GpuMat src, GpuMat dst, GpuMat M, Size dsize, int flags, int borderMode, Scalar borderValue);
 #ifdef __cplusplus
 }
 #endif
