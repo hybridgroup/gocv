9faa6a86b25221c0195d775a6b516f973b760179
Ron Evans <ron@hybridgroup.com>
2019-10-12 00:01:40 +0200

    openvino: updates needed for 2019 R3
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

--- openvino/pvl/pvl.go
+++
@@ -1,7 +0,0 @@
-// Package pvl is the GoCV wrapper around the Intel OpenVINO toolkit's
-// Photography vision Library (PVL).
-//
-// For further details, please see:
-// https://software.intel.com/en-us/openvino-toolkit
-//
-package pvl // import "gocv.io/x/gocv/openvino/pvl"
