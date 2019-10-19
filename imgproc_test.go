9bcb2cf96ce1b6f859c921504606044cbf9fb444
Domenico Luciani <domenicoleoneluciani@gmail.com>
2019-10-19 16:48:03 +0200

    Added ClipLine

--- imgproc_test.go
+++ imgproc_test.go
@@ -1219,6 +1219,13 @@ func TestWarpAffineWithParamsGocvLogo(t *testing.T) {
 	}
 }
 
+func TestClipLine(t *testing.T) {
+
+	if ok := ClipLine(image.Point{20, 20}, image.Point{5, 5}, image.Point{5, 5}); !ok {
+		t.Error("ClipLine(): is false")
+	}
+}
+
 func TestWatershed(t *testing.T) {
 	src := IMRead("images/gocvlogo.jpg", IMReadUnchanged)
 	if src.Empty() {
