93d8d4677542b7a89407b844213c6cbeda2f5d9b
Ron Evans <ron@hybridgroup.com>
2019-12-22 11:52:02 +0100

    opencv: update to OpenCV 4.2.0 release
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

--- version.go
+++ version.go
@@ -7,7 +7,7 @@ package gocv
 import "C"
 
 // GoCVVersion of this package, for display purposes.
-const GoCVVersion = "0.21.0"
+const GoCVVersion = "0.22.0-dev"
 
 // Version returns the current golang package version
 func Version() string {
