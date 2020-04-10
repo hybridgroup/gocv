a52e0632d7c9d2386c6b72b0be2d7cd4ab9d73cc
Ron Evans <ron@hybridgroup.com>
2020-04-10 10:21:49 +0200

    dnn: avoid parallel test runs
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

--- dnn_test.go
+++ dnn_test.go
@@ -7,263 +7,272 @@ import (
 	"testing"
 )
 
-func TestReadNet(t *testing.T) {
-	checkNet := func(t *testing.T, net Net) {
-		net.SetPreferableBackend(NetBackendDefault)
-		net.SetPreferableTarget(NetTargetCPU)
+func checkNet(t *testing.T, net Net) {
+	net.SetPreferableBackend(NetBackendDefault)
+	net.SetPreferableTarget(NetTargetCPU)
 
-		img := IMRead("images/space_shuttle.jpg", IMReadColor)
-		if img.Empty() {
-			t.Error("Invalid Mat in ReadNet test")
-		}
-		defer img.Close()
+	img := IMRead("images/space_shuttle.jpg", IMReadColor)
+	if img.Empty() {
+		t.Error("Invalid Mat in ReadNet test")
+	}
+	defer img.Close()
 
-		blob := BlobFromImage(img, 1.0, image.Pt(224, 224), NewScalar(0, 0, 0, 0), false, false)
-		if blob.Empty() {
-			t.Error("Invalid blob in ReadNet test")
-		}
-		defer blob.Close()
+	blob := BlobFromImage(img, 1.0, image.Pt(224, 224), NewScalar(0, 0, 0, 0), false, false)
+	if blob.Empty() {
+		t.Error("Invalid blob in ReadNet test")
+	}
+	defer blob.Close()
 
-		net.SetInput(blob, "data")
+	net.SetInput(blob, "data")
 
-		layer := net.GetLayer(0)
-		defer layer.Close()
+	layer := net.GetLayer(0)
+	defer layer.Close()
 
-		if layer.InputNameToIndex("notthere") != -1 {
-			t.Error("Invalid layer in ReadNet test")
-		}
-		if layer.OutputNameToIndex("notthere") != -1 {
-			t.Error("Invalid layer in ReadNet test")
-		}
-		if layer.GetName() != "_input" {
-			t.Errorf("Invalid layer name in ReadNet test: %s\n", layer.GetName())
-		}
-		if layer.GetType() != "" {
-			t.Errorf("Invalid layer type in ReadNet test: %s\n", layer.GetType())
-		}
-
-		ids := net.GetUnconnectedOutLayers()
-		if len(ids) != 1 {
-			t.Errorf("Invalid len output layers in ReadNet test: %d\n", len(ids))
-		}
-
-		lnames := net.GetLayerNames()
-		if len(lnames) != 142 {
-			t.Errorf("Invalid len layer names in ReadNet test: %d\n", len(lnames))
-		}
-
-		prob := net.ForwardLayers([]string{"prob"})
-		if len(prob) == 0 {
-			t.Error("Invalid len prob in ReadNet test")
-		}
-
-		if prob[0].Empty() {
-			t.Error("Invalid prob[0] in ReadNet test")
-		}
-
-		probMat := prob[0].Reshape(1, 1)
-		defer probMat.Close()
-		_, maxVal, minLoc, maxLoc := MinMaxLoc(probMat)
-
-		if round(float64(maxVal), 0.00005) != 0.9998 {
-			t.Errorf("ReadNet maxVal incorrect: %v\n", round(float64(maxVal), 0.00005))
-		}
-
-		if minLoc.X != 955 || minLoc.Y != 0 {
-			t.Errorf("ReadNet minLoc incorrect: %v\n", minLoc)
-		}
-
-		if maxLoc.X != 812 || maxLoc.Y != 0 {
-			t.Errorf("ReadNet maxLoc incorrect: %v\n", maxLoc)
-		}
-
-		perf := net.GetPerfProfile()
-		if perf == 0 {
-			t.Error("ReadNet GetPerfProfile error")
-		}
+	if layer.InputNameToIndex("notthere") != -1 {
+		t.Error("Invalid layer in ReadNet test")
+	}
+	if layer.OutputNameToIndex("notthere") != -1 {
+		t.Error("Invalid layer in ReadNet test")
+	}
+	if layer.GetName() != "_input" {
+		t.Errorf("Invalid layer name in ReadNet test: %s\n", layer.GetName())
+	}
+	if layer.GetType() != "" {
+		t.Errorf("Invalid layer type in ReadNet test: %s\n", layer.GetType())
 	}
 
+	ids := net.GetUnconnectedOutLayers()
+	if len(ids) != 1 {
+		t.Errorf("Invalid len output layers in ReadNet test: %d\n", len(ids))
+	}
+
+	lnames := net.GetLayerNames()
+	if len(lnames) != 142 {
+		t.Errorf("Invalid len layer names in ReadNet test: %d\n", len(lnames))
+	}
+
+	prob := net.ForwardLayers([]string{"prob"})
+	if len(prob) == 0 {
+		t.Error("Invalid len prob in ReadNet test")
+	}
+
+	if prob[0].Empty() {
+		t.Error("Invalid prob[0] in ReadNet test")
+	}
+
+	probMat := prob[0].Reshape(1, 1)
+	defer probMat.Close()
+	_, maxVal, minLoc, maxLoc := MinMaxLoc(probMat)
+
+	if round(float64(maxVal), 0.00005) != 0.9998 {
+		t.Errorf("ReadNet maxVal incorrect: %v\n", round(float64(maxVal), 0.00005))
+	}
+
+	if minLoc.X != 955 || minLoc.Y != 0 {
+		t.Errorf("ReadNet minLoc incorrect: %v\n", minLoc)
+	}
+
+	if maxLoc.X != 812 || maxLoc.Y != 0 {
+		t.Errorf("ReadNet maxLoc incorrect: %v\n", maxLoc)
+	}
+
+	perf := net.GetPerfProfile()
+	if perf == 0 {
+		t.Error("ReadNet GetPerfProfile error")
+	}
+}
+
+func TestReadNetDisk(t *testing.T) {
 	path := os.Getenv("GOCV_CAFFE_TEST_FILES")
 	if path == "" {
 		t.Skip("Unable to locate Caffe model files for tests")
 	}
 
-	t.Run("net from disk", func(t *testing.T) {
-		net := ReadNet(path+"/bvlc_googlenet.caffemodel", path+"/bvlc_googlenet.prototxt")
-		if net.Empty() {
-			t.Errorf("Unable to load Caffe model using ReadNet")
-		}
-		defer net.Close()
+	net := ReadNet(path+"/bvlc_googlenet.caffemodel", path+"/bvlc_googlenet.prototxt")
+	if net.Empty() {
+		t.Errorf("Unable to load Caffe model using ReadNet")
+	}
+	defer net.Close()
 
-		checkNet(t, net)
-	})
-
-	t.Run("net from memory", func(t *testing.T) {
-		bModel, err := ioutil.ReadFile(path + "/bvlc_googlenet.caffemodel")
-		if err != nil {
-			t.Errorf("Failed to load model from file: %v", err)
-		}
-		bConfig, err := ioutil.ReadFile(path + "/bvlc_googlenet.prototxt")
-		if err != nil {
-			t.Errorf("Failed to load config from file: %v", err)
-		}
-		net, err := ReadNetBytes("caffe", bModel, bConfig)
-		if err != nil {
-			t.Errorf("Failed to read net bytes: %v", err)
-		}
-		if net.Empty() {
-			t.Errorf("Unable to load Caffe model using ReadNetBytes")
-		}
-		defer net.Close()
-
-		checkNet(t, net)
-	})
+	checkNet(t, net)
 }
 
-func TestCaffe(t *testing.T) {
-	checkNet := func(t *testing.T, net Net) {
-		img := IMRead("images/space_shuttle.jpg", IMReadColor)
-		if img.Empty() {
-			t.Error("Invalid Mat in Caffe test")
-		}
-		defer img.Close()
-
-		blob := BlobFromImage(img, 1.0, image.Pt(224, 224), NewScalar(0, 0, 0, 0), false, false)
-		if blob.Empty() {
-			t.Error("Invalid blob in Caffe test")
-		}
-		defer blob.Close()
-
-		net.SetInput(blob, "data")
-		prob := net.Forward("prob")
-		defer prob.Close()
-		if prob.Empty() {
-			t.Error("Invalid prob in Caffe test")
-		}
-
-		probMat := prob.Reshape(1, 1)
-		defer probMat.Close()
-		_, maxVal, minLoc, maxLoc := MinMaxLoc(probMat)
-
-		if round(float64(maxVal), 0.00005) != 0.9998 {
-			t.Errorf("Caffe maxVal incorrect: %v\n", round(float64(maxVal), 0.00005))
-		}
-
-		if minLoc.X != 955 || minLoc.Y != 0 {
-			t.Errorf("Caffe minLoc incorrect: %v\n", minLoc)
-		}
-
-		if maxLoc.X != 812 || maxLoc.Y != 0 {
-			t.Errorf("Caffe maxLoc incorrect: %v\n", maxLoc)
-		}
-	}
-
+func TestReadNetMemory(t *testing.T) {
 	path := os.Getenv("GOCV_CAFFE_TEST_FILES")
 	if path == "" {
 		t.Skip("Unable to locate Caffe model files for tests")
 	}
 
-	t.Run("net from disk", func(t *testing.T) {
-		net := ReadNetFromCaffe(path+"/bvlc_googlenet.prototxt", path+"/bvlc_googlenet.caffemodel")
-		if net.Empty() {
-			t.Errorf("Unable to load Caffe model")
-		}
-		defer net.Close()
+	bModel, err := ioutil.ReadFile(path + "/bvlc_googlenet.caffemodel")
+	if err != nil {
+		t.Errorf("Failed to load model from file: %v", err)
+	}
+	bConfig, err := ioutil.ReadFile(path + "/bvlc_googlenet.prototxt")
+	if err != nil {
+		t.Errorf("Failed to load config from file: %v", err)
+	}
+	net, err := ReadNetBytes("caffe", bModel, bConfig)
+	if err != nil {
+		t.Errorf("Failed to read net bytes: %v", err)
+	}
+	if net.Empty() {
+		t.Errorf("Unable to load Caffe model using ReadNetBytes")
+	}
+	defer net.Close()
 
-		checkNet(t, net)
-	})
-
-	t.Run("net from memory", func(t *testing.T) {
-		bPrototxt, err := ioutil.ReadFile(path + "/bvlc_googlenet.prototxt")
-		if err != nil {
-			t.Errorf("Failed to load Caffe prototxt from file: %v", err)
-		}
-		bCaffeModel, err := ioutil.ReadFile(path + "/bvlc_googlenet.caffemodel")
-		if err != nil {
-			t.Errorf("Failed to load Caffe caffemodel from file: %v", err)
-		}
-		net, err := ReadNetFromCaffeBytes(bPrototxt, bCaffeModel)
-		if err != nil {
-			t.Errorf("Error reading caffe from bytes: %v", err)
-		}
-		if net.Empty() {
-			t.Errorf("Unable to load Caffe model")
-		}
-		defer net.Close()
-
-		checkNet(t, net)
-	})
+	checkNet(t, net)
 }
 
-func TestTensorflow(t *testing.T) {
-	checkNet := func(t *testing.T, net Net) {
-		img := IMRead("images/space_shuttle.jpg", IMReadColor)
-		if img.Empty() {
-			t.Error("Invalid Mat in Tensorflow test")
-		}
-		defer img.Close()
+func checkCaffeNet(t *testing.T, net Net) {
+	img := IMRead("images/space_shuttle.jpg", IMReadColor)
+	if img.Empty() {
+		t.Error("Invalid Mat in Caffe test")
+	}
+	defer img.Close()
 
-		blob := BlobFromImage(img, 1.0, image.Pt(224, 224), NewScalar(0, 0, 0, 0), true, false)
-		if blob.Empty() {
-			t.Error("Invalid blob in Tensorflow test")
-		}
-		defer blob.Close()
+	blob := BlobFromImage(img, 1.0, image.Pt(224, 224), NewScalar(0, 0, 0, 0), false, false)
+	if blob.Empty() {
+		t.Error("Invalid blob in Caffe test")
+	}
+	defer blob.Close()
 
-		net.SetInput(blob, "input")
-		prob := net.Forward("softmax2")
-		defer prob.Close()
-		if prob.Empty() {
-			t.Error("Invalid softmax2 in Tensorflow test")
-		}
-
-		probMat := prob.Reshape(1, 1)
-		defer probMat.Close()
-		_, maxVal, minLoc, maxLoc := MinMaxLoc(probMat)
-
-		if round(float64(maxVal), 0.00005) != 1.0 {
-			t.Errorf("Tensorflow maxVal incorrect: %v\n", round(float64(maxVal), 0.00005))
-		}
-
-		if minLoc.X != 481 || minLoc.Y != 0 {
-			t.Errorf("Tensorflow minLoc incorrect: %v\n", minLoc)
-		}
-
-		if maxLoc.X != 234 || maxLoc.Y != 0 {
-			t.Errorf("Tensorflow maxLoc incorrect: %v\n", maxLoc)
-		}
+	net.SetInput(blob, "data")
+	prob := net.Forward("prob")
+	defer prob.Close()
+	if prob.Empty() {
+		t.Error("Invalid prob in Caffe test")
 	}
 
+	probMat := prob.Reshape(1, 1)
+	defer probMat.Close()
+	_, maxVal, minLoc, maxLoc := MinMaxLoc(probMat)
+
+	if round(float64(maxVal), 0.00005) != 0.9998 {
+		t.Errorf("Caffe maxVal incorrect: %v\n", round(float64(maxVal), 0.00005))
+	}
+
+	if minLoc.X != 955 || minLoc.Y != 0 {
+		t.Errorf("Caffe minLoc incorrect: %v\n", minLoc)
+	}
+
+	if maxLoc.X != 812 || maxLoc.Y != 0 {
+		t.Errorf("Caffe maxLoc incorrect: %v\n", maxLoc)
+	}
+}
+
+func TestCaffeDisk(t *testing.T) {
+	path := os.Getenv("GOCV_CAFFE_TEST_FILES")
+	if path == "" {
+		t.Skip("Unable to locate Caffe model files for tests")
+	}
+
+	net := ReadNetFromCaffe(path+"/bvlc_googlenet.prototxt", path+"/bvlc_googlenet.caffemodel")
+	if net.Empty() {
+		t.Errorf("Unable to load Caffe model")
+	}
+	defer net.Close()
+
+	checkCaffeNet(t, net)
+}
+
+func TestCaffeMemory(t *testing.T) {
+	path := os.Getenv("GOCV_CAFFE_TEST_FILES")
+	if path == "" {
+		t.Skip("Unable to locate Caffe model files for tests")
+	}
+
+	bPrototxt, err := ioutil.ReadFile(path + "/bvlc_googlenet.prototxt")
+	if err != nil {
+		t.Errorf("Failed to load Caffe prototxt from file: %v", err)
+	}
+	bCaffeModel, err := ioutil.ReadFile(path + "/bvlc_googlenet.caffemodel")
+	if err != nil {
+		t.Errorf("Failed to load Caffe caffemodel from file: %v", err)
+	}
+	net, err := ReadNetFromCaffeBytes(bPrototxt, bCaffeModel)
+	if err != nil {
+		t.Errorf("Error reading caffe from bytes: %v", err)
+	}
+	if net.Empty() {
+		t.Errorf("Unable to load Caffe model")
+	}
+	defer net.Close()
+
+	checkCaffeNet(t, net)
+}
+
+func checkTensorflowNet(t *testing.T, net Net) {
+	img := IMRead("images/space_shuttle.jpg", IMReadColor)
+	if img.Empty() {
+		t.Error("Invalid Mat in Tensorflow test")
+	}
+	defer img.Close()
+
+	blob := BlobFromImage(img, 1.0, image.Pt(224, 224), NewScalar(0, 0, 0, 0), true, false)
+	if blob.Empty() {
+		t.Error("Invalid blob in Tensorflow test")
+	}
+	defer blob.Close()
+
+	net.SetInput(blob, "input")
+	prob := net.Forward("softmax2")
+	defer prob.Close()
+	if prob.Empty() {
+		t.Error("Invalid softmax2 in Tensorflow test")
+	}
+
+	probMat := prob.Reshape(1, 1)
+	defer probMat.Close()
+	_, maxVal, minLoc, maxLoc := MinMaxLoc(probMat)
+
+	if round(float64(maxVal), 0.00005) != 1.0 {
+		t.Errorf("Tensorflow maxVal incorrect: %v\n", round(float64(maxVal), 0.00005))
+	}
+
+	if minLoc.X != 481 || minLoc.Y != 0 {
+		t.Errorf("Tensorflow minLoc incorrect: %v\n", minLoc)
+	}
+
+	if maxLoc.X != 234 || maxLoc.Y != 0 {
+		t.Errorf("Tensorflow maxLoc incorrect: %v\n", maxLoc)
+	}
+}
+
+func TestTensorflowDisk(t *testing.T) {
 	path := os.Getenv("GOCV_TENSORFLOW_TEST_FILES")
 	if path == "" {
 		t.Skip("Unable to locate Tensorflow model file for tests")
 	}
 
-	t.Run("net from disk", func(t *testing.T) {
-		net := ReadNetFromTensorflow(path + "/tensorflow_inception_graph.pb")
-		if net.Empty() {
-			t.Errorf("Unable to load Tensorflow model")
-		}
-		defer net.Close()
+	net := ReadNetFromTensorflow(path + "/tensorflow_inception_graph.pb")
+	if net.Empty() {
+		t.Errorf("Unable to load Tensorflow model")
+	}
+	defer net.Close()
 
-		checkNet(t, net)
-	})
+	checkTensorflowNet(t, net)
+}
 
-	t.Run("net from memory", func(t *testing.T) {
-		b, err := ioutil.ReadFile(path + "/tensorflow_inception_graph.pb")
-		if err != nil {
-			t.Errorf("Failed to load tensorflow model from file: %v", err)
-		}
-		net, err := ReadNetFromTensorflowBytes(b)
-		if err != nil {
-			t.Errorf("Failed to load Tensorflow model from bytes: %v", err)
-		}
-		if net.Empty() {
-			t.Errorf("Unable to load Tensorflow model")
-		}
-		defer net.Close()
+func TestTensorflowMemory(t *testing.T) {
+	path := os.Getenv("GOCV_TENSORFLOW_TEST_FILES")
+	if path == "" {
+		t.Skip("Unable to locate Tensorflow model file for tests")
+	}
 
-		checkNet(t, net)
-	})
+	b, err := ioutil.ReadFile(path + "/tensorflow_inception_graph.pb")
+	if err != nil {
+		t.Errorf("Failed to load tensorflow model from file: %v", err)
+	}
+	net, err := ReadNetFromTensorflowBytes(b)
+	if err != nil {
+		t.Errorf("Failed to load Tensorflow model from bytes: %v", err)
+	}
+	if net.Empty() {
+		t.Errorf("Unable to load Tensorflow model")
+	}
+	defer net.Close()
+
+	checkTensorflowNet(t, net)
 }
 
 func TestBlobFromImages(t *testing.T) {
