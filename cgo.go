bf049d549f8710bd42b79ccd85aba7a05839f217
Tianqian Zhu <zhutianqian@gmail.com>
2020-04-20 15:38:43 +0800

    Add link flags for the photo module

--- cgo.go
+++ cgo.go
@@ -8,6 +8,6 @@ package gocv
 #cgo !windows pkg-config: opencv4
 #cgo CXXFLAGS:   --std=c++11
 #cgo windows  CPPFLAGS:   -IC:/opencv/build/install/include
-#cgo windows  LDFLAGS:    -LC:/opencv/build/install/x64/mingw/lib -lopencv_core430 -lopencv_face430 -lopencv_videoio430 -lopencv_imgproc430 -lopencv_highgui430 -lopencv_imgcodecs430 -lopencv_objdetect430 -lopencv_features2d430 -lopencv_video430 -lopencv_dnn430 -lopencv_xfeatures2d430 -lopencv_plot430 -lopencv_tracking430 -lopencv_img_hash430 -lopencv_calib3d430
+#cgo windows  LDFLAGS:    -LC:/opencv/build/install/x64/mingw/lib -lopencv_core430 -lopencv_face430 -lopencv_videoio430 -lopencv_imgproc430 -lopencv_highgui430 -lopencv_imgcodecs430 -lopencv_objdetect430 -lopencv_features2d430 -lopencv_video430 -lopencv_dnn430 -lopencv_xfeatures2d430 -lopencv_plot430 -lopencv_tracking430 -lopencv_img_hash430 -lopencv_calib3d430 -lopencv_photo430
 */
 import "C"
