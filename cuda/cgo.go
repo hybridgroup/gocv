d54c3bcae0adaef36df2a89dcfb94a591e6f2ce7
deadprogram <ron@hybridgroup.com>
2020-07-20 18:56:02 +0200

    all: update to OpenCV 4.4.0
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- cuda/cgo.go
+++ cuda/cgo.go
@@ -8,6 +8,6 @@ package cuda
 #cgo !windows pkg-config: opencv4
 #cgo CXXFLAGS:   --std=c++11
 #cgo windows  CPPFLAGS:   -IC:/opencv/build/install/include
-#cgo windows  LDFLAGS:    -LC:/opencv/build/install/x64/mingw/lib -lopencv_core430 -lopencv_face430 -lopencv_videoio430 -lopencv_imgproc430 -lopencv_highgui430 -lopencv_imgcodecs430 -lopencv_objdetect430 -lopencv_features2d430 -lopencv_video430 -lopencv_dnn430 -lopencv_xfeatures2d430 -lopencv_plot430 -lopencv_tracking430 -lopencv_img_hash430 -lopencv_calib3d430
+#cgo windows  LDFLAGS:    -LC:/opencv/build/install/x64/mingw/lib -lopencv_core440 -lopencv_face440 -lopencv_videoio440 -lopencv_imgproc440 -lopencv_highgui440 -lopencv_imgcodecs440 -lopencv_objdetect440 -lopencv_features2d440 -lopencv_video440 -lopencv_dnn440 -lopencv_xfeatures2d440 -lopencv_plot440 -lopencv_tracking440 -lopencv_img_hash440 -lopencv_calib3d440 -lopencv_bgsegm440
 */
 import "C"
