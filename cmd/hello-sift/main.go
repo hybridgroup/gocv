dda77bdf8522c094e20fcb8a77ee3bb5110435ee
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2020-07-31 14:14:04 +0200

    minor changes to CMD

--- cmd/hello-sift/main.go
+++ cmd/hello-sift/main.go
@@ -10,36 +10,44 @@ import (
 
 // you can run this example from root of this repository
 // with go run ./cmd/hello-sift /path/to/querry /path/to/train
+//
+// this is an example of Brute-Force Matching
+// with SIFT Descriptors and Ratio Test
 func main() {
 	if len(os.Args) != 3 {
-		println("Usage: app /path/to/querry /path/to/train")
+		fmt.Println("Usage: app /path/to/querry /path/to/train")
 	}
 
+	// opening querry image
 	querry := gocv.IMRead(os.Args[1], gocv.IMReadGrayScale)
 	defer querry.Close()
 
+	// opening train image
 	train := gocv.IMRead(os.Args[2], gocv.IMReadGrayScale)
 	defer train.Close()
 
-	println("images opened")
+	fmt.Println("images opened")
 
+	// creating new SIFT
 	sift := gocv.NewSIFT()
 	defer sift.Close()
 
-	println("sift created")
+	fmt.Println("sift created")
 
+	// detecting and computing keypoints using SIFT method
 	kp1, des1 := sift.DetectAndCompute(querry, gocv.NewMat())
 	kp2, des2 := sift.DetectAndCompute(train, gocv.NewMat())
 
-	println("detect and compute done")
+	fmt.Println("detect and compute done")
 
+	// finding K best matches for each descriptor
 	bf := gocv.NewBFMatcher()
 	matches := bf.KnnMatch(des1, des2, 2)
 
-	println("matches created")
+	fmt.Println("matches created")
 
+	// application of ratio test
 	var good []gocv.DMatch
-
 	for _, m := range matches {
 		if len(m) > 1 {
 			if m[0].Distance < 0.75*m[1].Distance {
@@ -48,52 +56,62 @@ func main() {
 		}
 	}
 
-	println("good selected")
-
+	fmt.Println("good selected")
 	fmt.Println(len(good))
 
+	// drawing keypoints on querry
 	gocv.DrawKeyPoints(querry, kp1, &querry, color.RGBA{
-		R: 255,
-		G: 0,
-		B: 0,
-		A: 0,
-	}, gocv.DrawDefault)
-
-	gocv.DrawKeyPoints(train, kp2, &train, color.RGBA{
-		R: 255,
-		G: 0,
-		B: 0,
-		A: 0,
-	}, gocv.DrawDefault)
-
-	c1 := color.RGBA{
-		R: 255,
-		G: 0,
-		B: 0,
-		A: 0,
-	}
-
-	c2 := color.RGBA{
 		R: 0,
 		G: 255,
 		B: 0,
 		A: 0,
+	}, gocv.DrawDefault)
+	// drawing keypoints on train
+	gocv.DrawKeyPoints(train, kp2, &train, color.RGBA{
+		R: 0,
+		G: 0,
+		B: 255,
+		A: 0,
+	}, gocv.DrawDefault)
+
+	// matches color
+	c1 := color.RGBA{
+		R: 255,
+		G: 0,
+		B: 0, // just blue
+		A: 0,
 	}
 
+	// point color
+	c2 := color.RGBA{
+		R: 100,
+		G: 0,
+		B: 100,
+		A: 0,
+	}
+
+	// creating empty mask
 	mask := make([]byte, len(good))
 
+	// new matrix for output image
 	out := gocv.NewMat()
+	// drawing matches
 	gocv.DrawMatches(querry, kp1, train, kp2, good, &out, c1, c2, mask, gocv.DrawDefault)
 
+	// creating windows with images
 	window1 := gocv.NewWindow("Query")
 	window1.IMShow(querry)
+	defer window1.Close()
 
 	window2 := gocv.NewWindow("Train")
 	window2.IMShow(train)
+	defer window2.Close()
 
 	window3 := gocv.NewWindow("Output")
 	window3.IMShow(out)
+	defer window3.Close()
 
 	window1.WaitKey(0)
 	window2.WaitKey(0)
+	window3.WaitKey(0)
 }
