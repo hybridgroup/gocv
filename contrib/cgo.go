6cf0b384341e9aca9d90375f2d5bcf43a4881dd6
deadprogram <ron@hybridgroup.com>
2020-10-15 12:13:55 +0200

    opencv: update to release 4.5.0 for Windows
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- contrib/cgo.go
+++ contrib/cgo.go
@@ -8,6 +8,6 @@ package contrib
 #cgo !windows pkg-config: opencv4
 #cgo CXXFLAGS:   --std=c++11
 #cgo windows  CPPFLAGS:   -IC:/opencv/build/install/include
-#cgo windows  LDFLAGS:    -LC:/opencv/build/install/x64/mingw/lib -lopencv_core440 -lopencv_face440 -lopencv_videoio440 -lopencv_imgproc440 -lopencv_highgui440 -lopencv_imgcodecs440 -lopencv_objdetect440 -lopencv_features2d440 -lopencv_video440 -lopencv_dnn440 -lopencv_xfeatures2d440 -lopencv_plot440 -lopencv_tracking440 -lopencv_img_hash440 -lopencv_calib3d440 -lopencv_bgsegm440
+#cgo windows  LDFLAGS:    -LC:/opencv/build/install/x64/mingw/lib -lopencv_core450 -lopencv_face450 -lopencv_videoio450 -lopencv_imgproc450 -lopencv_highgui450 -lopencv_imgcodecs450 -lopencv_objdetect450 -lopencv_features2d450 -lopencv_video450 -lopencv_dnn450 -lopencv_xfeatures2d450 -lopencv_plot450 -lopencv_tracking450 -lopencv_img_hash450 -lopencv_calib3d450 -lopencv_bgsegm450
 */
 import "C"
