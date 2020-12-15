71f6f24a6673843b99cca8c8ea4e36a56ae4558a
lijingfeng <lijingfeng@laiye.com>
2020-12-15 20:05:00 +0800

    complete seamlessclone function

--- photo.cpp
+++ photo.cpp
@@ -1,6 +1,18 @@
 #include "photo.h"
 
+void ColorChange(Mat src, Mat mask, Mat dst, float red_mul, float green_mul, float blue_mul) {
+    cv::colorChange(*src, *mask, *dst, red_mul, green_mul, blue_mul);
+}
+
+void IlluminationChange(Mat src, Mat mask, Mat dst, float alpha, float beta) {
+    cv::illuminationChange(*src, *mask, *dst, alpha, beta);
+}
+
 void SeamlessClone(Mat src, Mat dst, Mat mask, Point p, Mat blend, int flags) {
     cv::Point pt(p.x, p.y);
     cv::seamlessClone(*src, *dst, *mask, pt, *blend, flags);
+}
+
+void TextureFlattening(Mat src, Mat mask, Mat dst, float low_threshold, float high_threshold, int kernel_size) {
+    cv::textureFlattening(*src, *mask, *dst, low_threshold, high_threshold, kernel_size);
 }
--- photo.h
+++ photo.h
@@ -9,7 +9,13 @@ extern "C" {
 
 #include "core.h"
 
+void ColorChange(Mat src, Mat mask, Mat dst, float red_mul, float green_mul, float blue_mul);
+
 void SeamlessClone(Mat src, Mat dst, Mat mask, Point p, Mat blend, int flags);
+
+void IlluminationChange(Mat src, Mat mask, Mat dst, float alpha, float beta);
+
+void TextureFlattening(Mat src, Mat mask, Mat dst, float low_threshold, float high_threshold, int kernel_size);
 
 #ifdef __cplusplus
 }
