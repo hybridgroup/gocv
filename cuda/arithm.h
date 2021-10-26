6385a2db30ebc3f0ab2d7aa12fb67bd56e7eb7e2
deadprogram <ron@hybridgroup.com>
2021-10-26 22:14:56 +0200

    cuda: implement a bunch of per-element operations
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- cuda/arithm.h
+++ cuda/arithm.h
@@ -12,6 +12,21 @@ extern "C" {
 #include "cuda.h"
 
 void GpuAbs(GpuMat src, GpuMat dst, Stream s);
+void GpuAbsDiff(GpuMat src1, GpuMat src2, GpuMat dst, Stream s);
+void GpuAdd(GpuMat src1, GpuMat src2, GpuMat dst, Stream s);
+void GpuBitwiseAnd(GpuMat src1, GpuMat src2, GpuMat dst, Stream s);
+void GpuBitwiseNot(GpuMat src, GpuMat dst, Stream s);
+void GpuBitwiseOr(GpuMat src1, GpuMat src2, GpuMat dst, Stream s);
+void GpuBitwiseXor(GpuMat src1, GpuMat src2, GpuMat dst, Stream s);
+void GpuDivide(GpuMat src1, GpuMat src2, GpuMat dst, Stream s);
+void GpuExp(GpuMat src, GpuMat dst, Stream s);
+void GpuLog(GpuMat src, GpuMat dst, Stream s);
+void GpuMax(GpuMat src1, GpuMat src2, GpuMat dst, Stream s);
+void GpuMin(GpuMat src1, GpuMat src2, GpuMat dst, Stream s);
+void GpuMultiply(GpuMat src1, GpuMat src2, GpuMat dst, Stream s);
+void GpuSqr(GpuMat src, GpuMat dst, Stream s);
+void GpuSqrt(GpuMat src, GpuMat dst, Stream s);
+void GpuSubtract(GpuMat src1, GpuMat src2, GpuMat dst, Stream s);
 void GpuThreshold(GpuMat src, GpuMat dst, double thresh, double maxval, int typ, Stream s);
 void GpuFlip(GpuMat src, GpuMat dst, int flipCode, Stream s);
 
