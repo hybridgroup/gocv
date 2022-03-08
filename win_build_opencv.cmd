014a02763cdc248ee9c276edda29bd5c48d5478a
deadprogram <ron@hybridgroup.com>
2022-03-08 19:00:32 +0100

    all: use mingw-w64 8.1.0 for protobuf compile
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- win_build_opencv.cmd
+++ win_build_opencv.cmd
@@ -31,7 +31,7 @@ echo.
 echo on
 
 cd /D C:\opencv\build
-set PATH=%PATH%;C:\Program Files (x86)\CMake\bin;C:\mingw-w64\x86_64-6.3.0-posix-seh-rt_v5-rev1\mingw64\bin
+set PATH=%PATH%;C:\Program Files (x86)\CMake\bin;C:\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0\mingw64\bin
 if [%1]==[static] (
   echo Build static opencv
   set enable_shared=OFF
