389e3afbe21486873221012af0e4fcb3aeadd026
deadprogram <ron@hybridgroup.com>
2022-05-26 19:47:09 +0200

    contrib/ximgproc: add anisotropicDiffusion() function
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- contrib/ximgproc_test.go
+++ contrib/ximgproc_test.go
@@ -5,6 +5,19 @@ import (
 	"testing"
 )
 
+func TestAnisotropicDiffusion(t *testing.T) {
+	src := gocv.NewMatWithSize(200, 200, gocv.MatTypeCV8UC3)
+	defer src.Close()
+	dst := gocv.NewMat()
+	defer dst.Close()
+
+	AnisotropicDiffusion(src, &dst, 0.5, 0.5, 100)
+
+	if src.Empty() || dst.Rows() != src.Rows() {
+		t.Error("invalid AnisotropicDiffusion test")
+	}
+}
+
 func TestEdgePreservingFilter(t *testing.T) {
 	src := gocv.NewMatWithSize(200, 200, gocv.MatTypeCV8UC3)
 	defer src.Close()
