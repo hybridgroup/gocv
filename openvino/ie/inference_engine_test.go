23383f6df9eeb583c2bcc95ee0c873fb74e8b3f1
Doupamo Eradiri <eradiri2@gmail.com>
2022-07-19 20:54:59 +0900

    openvino: Fix OpenvinoVersion dangling pointer
    Fixed dangling pointer in OpenVinoVersion by copying the version
    string into a malloc'd buffer and adding a corresponding 'defer C.free'
    call to inference_engine.go.
    
    Also updated the test to ensure the version string length is sensible.
    A warning is printed if '2022.1' is not included in the version string.

--- openvino/ie/inference_engine_test.go
+++ openvino/ie/inference_engine_test.go
@@ -1,12 +1,22 @@
 package ie
 
 import (
+	"strings"
 	"testing"
 )
 
 func TestInferenceEngineVersion(t *testing.T) {
-	if Version() == "" {
+	v := Version()
+	expected := "2022.1"
+	if v == "" || len(v) < len(expected) {
 		t.Error("Invalid IE Version")
 		return
+	}
+
+	// Different platforms can have different strings, only warn if expected
+	// version string does not match.
+	// eg. archlinux: 2.1.custom_makepkg_cdb9bec7210f8c24fde3e416c7ada820faaaa23e
+	if !strings.Contains(v, expected) {
+		t.Log("Unexpected IE Version: ", v)
 	}
 }
