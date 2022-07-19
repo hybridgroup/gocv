d8656b93d5798d53138b963be5e6054fb8862e97
Doupamo Eradiri <eradiri2@gmail.com>
2022-07-19 18:54:29 +0900

    openvino: Update env.sh and README.md for 2022.1

--- openvino/env.sh
+++ openvino/env.sh
@@ -1,4 +1,4 @@
 export CGO_CXXFLAGS="--std=c++11"
-export CGO_CPPFLAGS="-I${INTEL_CVSDK_DIR}/opencv/include -I${INTEL_CVSDK_DIR}/deployment_tools/inference_engine/include"
-export CGO_LDFLAGS="-L${INTEL_CVSDK_DIR}/opencv/lib -L${INTEL_CVSDK_DIR}/deployment_tools/inference_engine/lib/intel64 -lpthread -ldl -linference_engine -lopencv_core -lopencv_videoio -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_objdetect -lopencv_features2d -lopencv_video -lopencv_dnn -lopencv_calib3d -lopencv_photo"
+export CGO_CPPFLAGS="-I${INTEL_OPENVINO_DIR}/extras/opencv/include -I${INTEL_OPENVINO_DIR}/runtime/include -I${INTEL_OPENVINO_DIR}/runtime/include/ie"
+export CGO_LDFLAGS="-L${INTEL_OPENVINO_DIR}/extras/opencv/lib -L${INTEL_OPENVINO_DIR}/runtime/lib/intel64 -lpthread -ldl -lopenvino -lopencv_core -lopencv_videoio -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_objdetect -lopencv_features2d -lopencv_video -lopencv_dnn -lopencv_calib3d -lopencv_photo"
 export PKG_CONFIG_PATH=/usr/lib64/pkgconfig
