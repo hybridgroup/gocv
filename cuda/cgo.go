396d967dd1d445702e067ca88ca08df7689ccdb5
deadprogram <ron@hybridgroup.com>
2023-10-11 18:43:34 +0200

    all: update to OpenCV 4.8.1
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- cuda/cgo.go
+++ cuda/cgo.go
@@ -9,6 +9,6 @@ package cuda
 #cgo !windows pkg-config: opencv4
 #cgo CXXFLAGS:   --std=c++11
 #cgo windows  CPPFLAGS:   -IC:/opencv/build/install/include
-#cgo windows  LDFLAGS:    -LC:/opencv/build/install/x64/mingw/lib -lopencv_core480 -lopencv_face480 -lopencv_videoio480 -lopencv_imgproc480 -lopencv_highgui480 -lopencv_imgcodecs480 -lopencv_objdetect480 -lopencv_features2d480 -lopencv_video480 -lopencv_dnn480 -lopencv_xfeatures2d480 -lopencv_plot480 -lopencv_tracking480 -lopencv_img_hash480 -lopencv_calib3d480 -lopencv_bgsegm480 -lopencv_aruco480 -lopencv_wechat_qrcode480 -lopencv_ximgproc480
+#cgo windows  LDFLAGS:    -LC:/opencv/build/install/x64/mingw/lib -lopencv_core481 -lopencv_face481 -lopencv_videoio481 -lopencv_imgproc481 -lopencv_highgui481 -lopencv_imgcodecs481 -lopencv_objdetect481 -lopencv_features2d481 -lopencv_video481 -lopencv_dnn481 -lopencv_xfeatures2d481 -lopencv_plot481 -lopencv_tracking481 -lopencv_img_hash481 -lopencv_calib3d481 -lopencv_bgsegm481 -lopencv_aruco481 -lopencv_wechat_qrcode481 -lopencv_ximgproc481
 */
 import "C"
