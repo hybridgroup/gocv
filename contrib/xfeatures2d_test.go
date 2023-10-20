acdb14632109c4610ac6300e50aae1cc25d08a59
Pericles Telemachou <pericles@outlook.com.au>
2023-10-20 11:18:06 +1100

    Add missing `Compute` test to `TestSURFWithParams`

--- contrib/xfeatures2d_test.go
+++ contrib/xfeatures2d_test.go
@@ -77,12 +77,20 @@ func TestSURFWithParams(t *testing.T) {
 	mask := gocv.NewMat()
 	defer mask.Close()
 
-	kp2, desc := si.DetectAndCompute(img, mask)
-	if len(kp2) == 512 {
-		t.Errorf("Invalid KeyPoint array in SURF DetectAndCompute: %d", len(kp2))
+	kpc, desc := si.Compute(img, mask, kp)
+	if len(kpc) < 512 {
+		t.Errorf("Invalid KeyPoint array in SURF Compute: %d", len(kpc))
+	}
+	if desc.Empty() {
+		t.Error("Invalid Mat desc in SURF Compute")
 	}
 
-	if desc.Empty() {
+	kpdc, desc2 := si.DetectAndCompute(img, mask)
+	defer desc2.Close()
+	if len(kpdc) < 512 {
+		t.Errorf("Invalid KeyPoint array in SURF DetectAndCompute: %d", len(kpdc))
+	}
+	if desc2.Empty() {
 		t.Error("Invalid Mat desc in SURF DetectAndCompute")
 	}
 }
