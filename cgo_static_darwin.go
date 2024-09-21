9dab401668f14d255045be2037c4de47cf15491d
deadprogram <ron@hybridgroup.com>
2024-09-21 09:15:14 +0200

    cgo: add options for linux arm64 and separate darwin builds
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ cgo_static_darwin.go
@@ -0,0 +1,11 @@
+//go:build !customenv && static && darwin
+
+package gocv
+
+// Changes here should be mirrored in contrib/cgo_static_darwin.go and cuda/cgo_static_darwin.go.
+
+/*
+#cgo CXXFLAGS: --std=c++11
+#cgo pkg-config: --static opencv4
+*/
+import "C"
