4987d75b8b2410d886bcbe5e1311ee7e2fc08aec
samuel-jimenez <53528911+samuel-jimenez@users.noreply.github.com>
2025-04-25 16:07:36 -0500

    bugfix: cd back to correct path

--- win_build_opencv.ps1
+++ win_build_opencv.ps1
@@ -48,6 +48,5 @@ cmake C:\opencv\opencv-4.11.0 -G "MinGW Makefiles" -BC:\opencv\build -DENABLE_CX
 mingw32-make -j $env:NUMBER_OF_PROCESSORS
 mingw32-make install
 
-cd $env:GOPATH/pkg/mod/gocv.io/x/gocv@v0.41.0
-
+cd $PSScriptRoot
 go run ./cmd/version/main.go
