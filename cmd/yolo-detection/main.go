21c5b08634a7f21e61726b2a4a1faae5205c77f5
swdee <shanon@bokumail.com>
2025-12-21 10:50:39 +1300

    moved coconet class from seperate file into main program to avoid runtime error

--- cmd/yolo-detection/main.go
+++ cmd/yolo-detection/main.go
@@ -20,6 +20,19 @@ import (
 	"gocv.io/x/gocv"
 )
 
+// COCONet class names
+var classes = []string{
+	"person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat",
+	"traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse",
+	"sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie",
+	"suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove",
+	"skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon",
+	"bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut",
+	"cake", "chair", "couch", "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse",
+	"remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book",
+	"clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush",
+}
+
 func main() {
 	if len(os.Args) < 3 {
 		fmt.Println("How to run:\nyolo-detection [videosource] [modelfile] ([backend] [device])")
