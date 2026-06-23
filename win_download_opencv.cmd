70c7bc22536816597d5c54aba8827bb07314a4b1
swdee <shanon@bokumail.com>
2026-06-24 00:29:27 +1200

    update windows build files to build version 5.0.0

--- win_download_opencv.cmd
+++ win_download_opencv.cmd
@@ -3,6 +3,9 @@
 REM Save the starting directory
 set "START_DIR=%CD%"
 
+REM Use variables for version
+set "OPENCV_VERSION=5.0.0"
+
 REM Set install directory, default to C:\opencv if not provided
 set "INSTALL_DIR=%~1"
 if "%INSTALL_DIR%"=="" set "INSTALL_DIR=C:\opencv"
@@ -17,18 +20,18 @@ echo.
 REM This is why there is no progress bar:
 REM https://github.com/PowerShell/PowerShell/issues/2138
 
-REM Downloading: opencv-4.13.0.zip [91MB]
-powershell -command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; $ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri 'https://github.com/opencv/opencv/archive/4.13.0.zip' -OutFile \"%INSTALL_DIR%\opencv-4.13.0.zip\""
+REM Downloading: opencv
+powershell -command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; $ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri 'https://github.com/opencv/opencv/archive/%OPENCV_VERSION%.zip' -OutFile \"%INSTALL_DIR%\opencv-%OPENCV_VERSION%.zip\""
 echo Extracting...
-powershell -command "$ProgressPreference = 'SilentlyContinue'; Expand-Archive -Path \"%INSTALL_DIR%\opencv-4.13.0.zip\" -DestinationPath \"%INSTALL_DIR%\""
-del "%INSTALL_DIR%\opencv-4.13.0.zip" /q
+powershell -command "$ProgressPreference = 'SilentlyContinue'; Expand-Archive -Path \"%INSTALL_DIR%\opencv-%OPENCV_VERSION%.zip\" -DestinationPath \"%INSTALL_DIR%\""
+del "%INSTALL_DIR%\opencv-%OPENCV_VERSION%.zip" /q
 echo.
 
-echo Downloading: opencv_contrib-4.13.0.zip [58MB]
-powershell -command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; $ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri 'https://github.com/opencv/opencv_contrib/archive/4.13.0.zip' -OutFile \"%INSTALL_DIR%\opencv_contrib-4.13.0.zip\""
+echo Downloading: opencv_contrib
+powershell -command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; $ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri 'https://github.com/opencv/opencv_contrib/archive/%OPENCV_VERSION%.zip' -OutFile \"%INSTALL_DIR%\opencv_contrib-%OPENCV_VERSION%.zip\""
 echo Extracting...
-powershell -command "$ProgressPreference = 'SilentlyContinue'; Expand-Archive -Path \"%INSTALL_DIR%\opencv_contrib-4.13.0.zip\" -DestinationPath \"%INSTALL_DIR%\""
-del "%INSTALL_DIR%\opencv_contrib-4.13.0.zip" /q
+powershell -command "$ProgressPreference = 'SilentlyContinue'; Expand-Archive -Path \"%INSTALL_DIR%\opencv_contrib-%OPENCV_VERSION%.zip\" -DestinationPath \"%INSTALL_DIR%\""
+del "%INSTALL_DIR%\opencv_contrib-%OPENCV_VERSION%.zip" /q
 echo.
 
 echo Done with downloading and extracting sources.
